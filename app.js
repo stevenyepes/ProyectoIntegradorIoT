3f18b7b15d4c20aa958cf0ee690fac18f9c25be5
steven <jhonatan.yepes@udea.edu.co>
2016-05-10 17:10:51 -0500

    Expuesto modulo para a coneccion

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var five = require("johnny-five");
+var awsConeccion = require("./pi.js")
 var board = new five.Board();
 var ultraBaseline = 20;
 var ultraThreshold = 3;
@@ -10,7 +11,7 @@ board.on("ready", function() {
   var proximity = new five.Proximity({
     controller: "HCSR04",
     pin: 7,
-    freq: 3000
+    freq: 60000
   });
 
    // Initialize the RGB LED
@@ -31,7 +32,7 @@ board.on("ready", function() {
 
   proximity.on("change", ultraChange);
 
- 
+
 });
 
 
@@ -40,18 +41,73 @@ function ultraChange(){
 
 	var cm = this.cm;
 
-	
+
 	if(Math.abs(cm - ultraBaseline) > ultraThreshold){
-		
+
            console.log("Alerta, la distancia cambió");
            // Turn it on and set the initial color
            led.on();
            led.color("#00FF00");
+           awsConeccion.thingShadows.on('connect', function() {
+             console.log("Connected...");
+             console.log("Registering...");
+             awsConeccion.thingShadows.register( awsConeccion.myThingName );
 
-		
+             // An update right away causes a timeout error, so we wait about 2 seconds
+             setTimeout( function() {
+               console.log("Updating my IP address...");
+               clientTokenIP = awsConeccion.thingShadows.update(awsConeccion.myThingName, awsConeccion.mythingstate);
+               console.log("Update:" + clientTokenIP);
+             }, 2500 );
+
+
+             // Code below just logs messages for info/debugging
+             awsConeccion.thingShadows.on('status',
+               function(thingName, stat, clientToken, stateObject) {
+                  console.log('received '+stat+' on '+thingName+': '+
+                              JSON.stringify(stateObject));
+               });
+
+             awsConeccion.thingShadows.on('update',
+                 function(thingName, stateObject) {
+                    console.log('received update '+' on '+thingName+': '+
+                                JSON.stringify(stateObject));
+                 });
+
+             awsConeccion.thingShadows.on('delta',
+                 function(thingName, stateObject) {
+                    console.log('received delta '+' on '+thingName+': '+
+                                JSON.stringify(stateObject));
+                 });
+
+             awsConeccion.thingShadows.on('timeout',
+                 function(thingName, clientToken) {
+                    console.log('received timeout for '+ clientToken)
+                 });
+
+             awsConeccion.thingShadows
+               .on('close', function() {
+                 console.log('close');
+               });
+             awsConeccion.thingShadows
+               .on('reconnect', function() {
+                 console.log('reconnect');
+               });
+             awsConeccion.thingShadows
+               .on('offline', function() {
+                 console.log('offline');
+               });
+             awsConeccion.thingShadows
+               .on('error', function(error) {
+                 console.log('error', error);
+               });
+
+           });
+
+
 	}else {
 	  led.color("#FF0000");
 	  console.log("Todo normal")
 	}
-	
+
 }
--- pi.js
+++ pi.js
@@ -1,3 +1,5 @@
+
+
 var awsIot = require('aws-iot-device-sdk');
 
 var myThingName = 'Prueba1';
@@ -21,65 +23,9 @@ mythingstate = {
 
 var networkInterfaces = require( 'os' ).networkInterfaces( );
 
-var distancia = 16 // htttp.get(blablabla)
-
-if(distancia <= 15){ //cm
-   mythingstate["state"]["reported"]["mensaje"] = "Alerta, Demasiado cerca"   
-}
 mythingstate["state"]["reported"]["ip"] = networkInterfaces['wlan0'][0]['address'];
 
-thingShadows.on('connect', function() {
-  console.log("Connected...");
-  console.log("Registering...");
-  thingShadows.register( myThingName );
 
-  // An update right away causes a timeout error, so we wait about 2 seconds
-  setTimeout( function() {
-    console.log("Updating my IP address...");
-    clientTokenIP = thingShadows.update(myThingName, mythingstate);
-    console.log("Update:" + clientTokenIP);
-  }, 2500 );
-
-
-  // Code below just logs messages for info/debugging
-  thingShadows.on('status',
-    function(thingName, stat, clientToken, stateObject) {
-       console.log('received '+stat+' on '+thingName+': '+
-                   JSON.stringify(stateObject));
-    });
-
-  thingShadows.on('update',
-      function(thingName, stateObject) {
-         console.log('received update '+' on '+thingName+': '+
-                     JSON.stringify(stateObject));
-      });
-
-  thingShadows.on('delta',
-      function(thingName, stateObject) {
-         console.log('received delta '+' on '+thingName+': '+
-                     JSON.stringify(stateObject));
-      });
-
-  thingShadows.on('timeout',
-      function(thingName, clientToken) {
-         console.log('received timeout for '+ clientToken)
-      });
-
-  thingShadows
-    .on('close', function() {
-      console.log('close');
-    });
-  thingShadows
-    .on('reconnect', function() {
-      console.log('reconnect');
-    });
-  thingShadows
-    .on('offline', function() {
-      console.log('offline');
-    });
-  thingShadows
-    .on('error', function(error) {
-      console.log('error', error);
-    });
-
-});
+module.export.thingShadows = thingShadows;
+module.export.mythingstate = mythingstate;
+module.export.myThingName = myThingName;
